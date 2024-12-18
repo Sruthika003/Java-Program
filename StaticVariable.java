4f7f97fb0cd68d2067d8f8003dfc2ebccfff2a0b
Sruthika003 <sruthika3042003@gmail.com>
2024-12-18 21:26:33 +0530

    Add files via upload

---
+++ StaticVariable.java
@@ -0,0 +1,15 @@
+package day2Excercise;
+
+public class StaticVariable {
+static int a = 10;
+ 
+ static void square() {
+	 int c= a*a;
+	 System.out.println(c);
+ }
+	public static void main(String[] args) {
+	square();
+
+	}
+
+}
