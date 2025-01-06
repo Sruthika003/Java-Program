a1150f8930cf2856ac689ff8814082745dbca13e
Sruthika003 <sruthika3042003@gmail.com>
2025-01-06 22:09:46 +0530

    Add files via upload

--- LocalVariable.java
+++ LocalVariable.java
@@ -1,17 +1,22 @@
-package day2Excercise;
+package day8Exercise;
+class Student{
+	String studentName;
+	int StudentAge;
+	String StudentDepartment;
 
+    public Student(String name,int age,String dept) {
+    	this.studentName = name;
+    	this.StudentAge = age;
+    	this.StudentDepartment = dept;
+    }
+}
 public class LocalVariable {
-	
-	public int multiple() {
-	int a = 10;
-	int b = 40;
-	return a * b;
-	}
-	
 
 	public static void main(String[] args) {
-		LocalVariable var = new LocalVariable();
-	System.out.println(var.multiple());
+		Student obj = new Student("Sruthika",21,"Food");
+		System.out.println("student name is "+ obj.studentName);
+		System.out.println("student age is "+obj.StudentAge);
+		System.out.println("student department is "+ obj.StudentAge);
 
 	}
 
